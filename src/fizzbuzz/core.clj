8ae08d6c7cd9504af86a51d992ffcfefa9d4e939
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2019-02-06 00:46:26 -0200

    Fizzbuzz finished

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,6 +1,13 @@
 (ns fizzbuzz.core)
 
-(defn foo
+(defn fizzbuzz [n]
+  (cond
+    (and (= 0 (mod n 5)) (= 0 (mod n 3))) "FizzBuzz"
+    (= 0 (mod n 3))      "Fizz"
+    (= 0 (mod n 5))      "Buzz"
+    :else n))
+
+(defn main
   "I don't do a whole lot."
   [x]
-  (println x "Hello, World!"))
+  (println (map fizzbuzz (range (Integer/parseInt x)))))
--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -1,7 +1,15 @@
 (ns fizzbuzz.core-test
   (:require [clojure.test :refer :all]
-            [fizzbuzz.core :refer :all]))
+            [fizzbuzz.core :as c]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest fizzbuzz
+  (testing "Print fizz when number is divisible by 3"
+    (is (= "Fizz" (c/fizzbuzz 3))))
+  (testing "Return number when not visible by 3"
+    (is (= 4 (c/fizzbuzz 4))))
+  (testing "Print buzz when number is divisible by 5"
+    (is (= "Buzz" (c/fizzbuzz 5))))
+  (testing "Return number when not visible by 5"
+    (is (= 7 (c/fizzbuzz 7))))
+  (testing "Print fizzbuzz when number is divisible by 5 and by 3"
+    (is (= "FizzBuzz" (c/fizzbuzz 15)))))
